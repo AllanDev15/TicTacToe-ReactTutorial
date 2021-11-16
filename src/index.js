4dbb2443035434c24e8434bc7fd52739cca949c0
Allan_PC <Allan.FL.15@live.com>
2021-11-16 10:16:23 -0600

    extra - using a loop for render the board component

--- src/index.js
+++ src/index.js
@@ -12,29 +12,20 @@ function Square(props) {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
+    return <Square key={i} value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    let board = [];
+    let squareNum = 0;
+    for (let i = 0; i < 3; i++) {
+      board.push([<div key={i + '' + i} className="board-row"></div>]);
+      for (let j = 0; j < 3; j++) {
+        board[i].push(this.renderSquare(squareNum));
+        squareNum++;
+      }
+    }
+    return <div>{board}</div>;
   }
 }
 
